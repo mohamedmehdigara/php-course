5ab3005b30be87d3108e2ca2ce1ea85d9776be55
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2020-09-03 16:07:42 +0100

    add tax calc function

--- index.php
+++ index.php
@@ -26,7 +26,11 @@
 		    if($value <= $credit ){
 		    	echo "<p>".$key."</p>"; 
 		    }
-	    }
+      }
+      $amount=800;
+      $taxRate=0.0825;
+      $addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
+      echo $addedTax;
     ?>
   </body>
 </html>
