94fe68825e7d5b698cef921159128a0583f2556c
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2020-09-03 16:00:46 +0100

    add conditional statement

--- index.php
+++ index.php
@@ -5,18 +5,28 @@
   </head>
   <body>
     <?php
-      $name = "PHP Store";
-      $credit = 1000; 
+	    $name = "PHP Store";
+			$credit = 1000; 
 
-      echo "<h1>Welcome to ".$name."!</h1>";
-      echo "<h2>You have $".$credit." in your wallet.</h2>";
+	    echo "<h1>Welcome to ".$name."!</h1>";
+	    echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-	    $products['Computer']=750;
+		  $products['Computer']=750;
 	    $products['Car']=15000;
 	    $products['iPhone']=1000;
 	    $products['Toaster']=75;
 
-	    echo "<p>A car costs $".$products['Car']."</p>";
+	    foreach($products as $key => $value){
+		    echo "<p>The ".$key." costs ".$value."</p>";
+	    }
+
+	    echo "<h2>Items you can afford</h2>";
+
+	    foreach($products as $key => $value){
+		    if($value <= $credit ){
+		    	echo "<p>".$key."</p>"; 
+		    }
+	    }
     ?>
   </body>
 </html>
